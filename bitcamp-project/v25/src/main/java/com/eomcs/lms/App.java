0c0d97d0cca9bad1ef4770235f50f3627d542588
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-01-21 18:18:56 +0900

    v25 복습

--- bitcamp-project/v25/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/v25/src/main/java/com/eomcs/lms/App.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
 import com.eomcs.lms.domain.Board;
 import com.eomcs.lms.domain.Lesson;
 import com.eomcs.lms.domain.Member;
@@ -16,157 +16,161 @@ import com.eomcs.lms.handler.MemberHandler;
 import com.eomcs.util.Prompt;
 
 public class App {
-  
-  static Scanner keyboard = new Scanner(System.in);
-  
-  // java.util.Stack 에서 제공하는 Iterator 객체는 FIFO 방식으로 값을 꺼내준다.
-  // LIFO 방식으로 꺼내는 Iterator가 필요하다면,
-  // java.util.Deque 구현체를 사용하라!
+  static java.io.InputStream inputStream = System.in;
+  static java.util.Scanner keyboard = new java.util.Scanner(inputStream);
   static Deque<String> commandStack = new ArrayDeque<>();
   static Queue<String> commandQueue = new LinkedList<>();
-  
+
   public static void main(String[] args) {
-    
     Prompt prompt = new Prompt(keyboard);
-    
-    // 단지 유지보수를 좋게 하기 위해 ArrayList와 LinkedList의 공통 분모를 뽑아서 
-    // 만든 클래스가 List이다. 
-    // List는 클래스는 실제 작업을 하는 클래스가 아니다.
-    // 그럼에도 불구하고 개발자가 다음과 같이 List 객체를 사용하려 한다면 막을 수 없다.
-    // => BoardHandler의 경우 아무런 작업을 수행하지 않을 것이다.
-    // => 왜? List 클래스에 정의된 메서드는 아무것도 하지 않는다.
-    //
-    //List<Board> boardList = new List<>();
-     
-    // 해결책?
-    // => 이렇게 generalization을 통해 만든 클래스의 경우 
-    //    서브 클래스에게 공통 분모를 물려주기 위한 용도로 사용된다.
-    // => 이런 류의 클래스는 직접 인스턴스를 생성하지 못하도록 해서 
-    //    직접 사용하는 것을 막아야 한다.
-    // => 이런 용도로 사용하는 문법이 "추상 클래스(abstract class)"이다.
-    //
-    // List 클래스(AbstractList로 이름 변경함)를 추상 클래스로 만들면,
-    // 다음과 같이 인스턴스를 생성할 수 없다.
-    // 아예 인스턴스 생성을 원천적으로 차단하는 효과가 있다.
-    //
-    //AbstractList<Board> boardList = new AbstractList<>(); // 컴파일 오류!
-    //
-    
-    // 받드시 AbstractList의 일반 하위 객체를 정의해야 한다.
-    //
-    LinkedList<Board> boardList = new LinkedList<>(); // 컴파일 오류!
+
+    List<Board> boardList = new ArrayList<>();
     BoardHandler boardHandler = new BoardHandler(prompt, boardList);
-    
-    ArrayList<Lesson> lessonList = new ArrayList<>();
+
+    List<Lesson> lessonList = new LinkedList<>();
     LessonHandler lessonHandler = new LessonHandler(prompt, lessonList);
-    
-    LinkedList<Member> memberList = new LinkedList<>();
+
+    List<Member> memberList = new LinkedList<>();
     MemberHandler memberHandler = new MemberHandler(prompt, memberList);
-    
+
     String command;
-    
+
     do {
-      System.out.print("\n명령> ");
+      System.out.println();
+      System.out.print("명령> ");
       command = keyboard.nextLine();
-      
-      if (command.length() == 0)
+      if(command.length() == 0)
         continue;
-      
       commandStack.push(command);
-      
       commandQueue.offer(command);
-      
+
       switch (command) {
-        case "/lesson/add":
+        case "/lesson/add" :
           lessonHandler.addLesson();
           break;
-        case "/lesson/list":
+
+        case "/lesson/list" :
           lessonHandler.listLesson();
           break;
-        case "/lesson/detail":
-          lessonHandler.detailLesson();
-          break;
-        case "/lesson/update":
+
+        case "/lesson/update" :
           lessonHandler.updateLesson();
           break;
-        case "/lesson/delete":
+
+        case "/lesson/delete" :
           lessonHandler.deleteLesson();
           break;
-        case "/member/add":
+
+        case "/lesson/detail" :
+          lessonHandler.detailLesson();
+          break;
+
+        case "/member/add" :
           memberHandler.addMember();
           break;
-        case "/member/list":
+
+        case "/member/list" :
           memberHandler.listMember();
           break;
-        case "/member/detail":
+
+          // 새로운 구문 추가 (detail) : 추가연습
+        case "/member/detail" :
           memberHandler.detailMember();
           break;
-        case "/member/update":
+
+        case "/member/update" :
           memberHandler.updateMember();
           break;
-        case "/member/delete":
+
+        case "/member/delete" :
           memberHandler.deleteMember();
           break;
-        case "/board/add":
+
+        case "/board/add" :
           boardHandler.addBoard();
           break;
-        case "/board/list":
+
+        case "/board/list" :
           boardHandler.listBoard();
           break;
-        case "/board/detail":
+
+          // 새로운 구문 추가 (detail) : 추가연습
+        case "/board/detail" :
           boardHandler.detailBoard();
-          break;  
-        case "/board/update":
-          boardHandler.updateBoard();
-          break; 
-        case "/board/delete":
-          boardHandler.deleteBoard();
-          break; 
-        case "history":
-          printCommandHistory(commandStack.iterator());
           break;
-        case "history2":
+
+        case "/board/update" :
+          boardHandler.updateBoard();
+          break;
+
+        case "/board/delete" :
+          boardHandler.deleteBoard();
+          break;
+
+        case "history" :
           printCommandHistory(commandQueue.iterator());
           break;
-        default:
-          if (!command.equalsIgnoreCase("quit")) {
+
+
+        case "history2" :
+          printCommandHistory(commandStack.iterator());
+          break;
+
+          
+        default :
+          if(!command.equalsIgnoreCase("quit"))
             System.out.println("실행할 수 없는 명령입니다.");
-          }
+          break;
       }
-      
+
     } while (!command.equalsIgnoreCase("quit"));
-    
-    System.out.println("안녕!");
-    
     keyboard.close();
+    System.out.println("...안녕!");
   }
-  
-  // 이전에는 Stack에서 값을 꺼내는 방법과 Queue에서 값을 꺼내는 방법이 다르기 때문에 
-  // printCommandHistory()와 printCommandHistory2() 메서드를 따로 정의했다.
-  // 이제 Stack과 Queue는 일관된 방식으로 값을 꺼내주는 Iterator가 있기 때문에
-  // 두 메서드를 하나로 합칠 수 있다.
-  // 파라미터로 Iterator를 받아서 처리하기만 하면 된다.
-  //
+
+
+  // Object.clone()의 shallow copy를 이용하여 스택 객체 복사하기
+  // 문제점 :
+  // 데이터가 실제 저장된 배열은 복제하지 않는다.
+  // 따라서, 배열의 값을 바꾸면 원본스택에도 영향을 받는다.
+
   private static void printCommandHistory(Iterator<String> iterator) {
     int count = 0;
-    while (iterator.hasNext()) {
+
+    while(iterator.hasNext()) {
       System.out.println(iterator.next());
-      count++;
       
-      if ((count % 5) == 0) {
-        System.out.print(":");
+      if((++count % 5) == 0) {
+        System.out.print(": (중지하고 싶으면 q)");
         String str = keyboard.nextLine();
-        if (str.equalsIgnoreCase("q")) {
+        if(str.equalsIgnoreCase("q"))
           break;
-        }
       }
     }
   }
-  
 }
 
+/*
+명령>
 
+명령> /lesson/add
+번호? 1
+수업명? 자바 프로젝트 실습
+수업내용? 자바 프로젝트를 통한 자바 언어 활용법 익히기
+시작일? 2019-01-02
+종료일? 2019-05-28
+총수업시간? 1000
+일수업시간? 8
+저장하였습니다.
 
+명령> /lesson/list
+1, 자바 프로젝트 실습     , 2019-01-02 ~ 2019-05-28, 1000
+2, 자바 프로그래밍 기초    , 2019-02-01 ~ 2019-02-28,  160
+3, 자바 프로그래밍 고급    , 2019-03-02 ~ 2019-03-30,  160
 
+명령> board/view
+실행할 수 없는 명령입니다.
 
-
+명령> quit
+안녕!
+ */
--- bitcamp-project/v25/src/main/java/com/eomcs/lms/handler/MemberHandler.java
+++ bitcamp-project/v25/src/main/java/com/eomcs/lms/handler/MemberHandler.java
@@ -3,128 +3,137 @@ package com.eomcs.lms.handler;
 import java.sql.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 import com.eomcs.lms.domain.Member;
 import com.eomcs.util.Prompt;
 
 public class MemberHandler {
-  
   List<Member> memberList;
-
-  Prompt prompt;
+  public Scanner input;
+  public Prompt prompt;
 
   public MemberHandler(Prompt prompt, List<Member> list) {
     this.prompt = prompt;
-    this.memberList = list;
+    memberList = list;
   }
-  
+  /*
+  public MemberHandler(Prompt prompt, int capacity) {
+    this.prompt = prompt;
+    memberList = new LinkedList<>(capacity);
+  }
+   */
+
+  ////////////////////////////////////////////////////////////////
+
   public void listMember() {
-    
-    // 목록에서 값을 꺼내는 일을 해 줄 Iterator 객체를 준비한다.
     Iterator<Member> iterator = memberList.iterator();
-    
-    // Iterator 객체를 통해 값을 꺼낸다.
-    while (iterator.hasNext()) {
+    while(iterator.hasNext()) {
       Member m = iterator.next();
-      System.out.printf("%d, %s, %s, %s, %s\n", 
-          m.getNo(), m.getName(), m.getEmail(), 
-          m.getTel(), m.getRegisteredDate());
+      System.out.printf("%1$d, %2$s , %3$s       , %4$s      , %5$tH:%5$tM:%5$tS\n",
+          m.getNo(), m.getName(),  m.getEmail(),  m.getTel() , m.getRegisteredDate() );
     }
   }
+
+  ////////////////////////////////////////////////////////////////
 
   public void addMember() {
-    Member member = new Member();
+    Member mem = new Member();
 
-    member.setNo(prompt.inputInt("번호? "));
-    member.setName(prompt.inputString("이름? "));
-    member.setEmail(prompt.inputString("이메일? "));
-    member.setPassword(prompt.inputString("암호? "));
-    member.setPhoto(prompt.inputString("사진? "));
-    member.setTel(prompt.inputString("전화? "));
-    member.setRegisteredDate(new Date(System.currentTimeMillis()));
-    
-    this.memberList.add(member);
-    
-    System.out.println("저장하였습니다.");
-  }
-  
-  public void detailMember() {
-    int index = indexOfMember(prompt.inputInt("번호? "));
-    
-    if (index == -1) {
-      System.out.println("해당 번호의 회원이 없습니다.");
-      return;
-    }
-    
-    Member member = this.memberList.get(index);
-    
-    System.out.printf("번호: %d\n", member.getNo());
-    System.out.printf("이름: %s\n", member.getName());
-    System.out.printf("이메일: %s\n", member.getEmail());
-    System.out.printf("암호: %s\n", member.getPassword());
-    System.out.printf("사진: %s\n", member.getPhoto());
-    System.out.printf("전화: %s\n", member.getTel());
-  }
-  
-  public void updateMember() {
-    int index = indexOfMember(prompt.inputInt("번호? "));
-    
-    if (index == -1) {
-      System.out.println("해당 번호의 회원이 없습니다.");
-      return;
-    }
-    
-    Member oldMember = this.memberList.get(index);
-    Member newMember = new Member();
-    
-    newMember.setNo(oldMember.getNo());
-    
-    newMember.setName(prompt.inputString(
-        String.format("이름(%s)? ", oldMember.getName()), 
-        oldMember.getName()));
+    mem.setNo(prompt.inputInt("번호? "));
+    mem.setName(prompt.inputString("이름? "));
+    mem.setEmail(prompt.inputString("이메일? "));
+    mem.setPassword(prompt.inputString("비밀번호? "));
+    mem.setPhoto(prompt.inputString("사진? "));
+    mem.setTel(prompt.inputString("전화? "));
+    mem.setRegisteredDate(new Date(System.currentTimeMillis()));
 
-    newMember.setEmail(prompt.inputString(
-        String.format("이메일(%s)? ", oldMember.getEmail()), 
-        oldMember.getEmail()));
-    
-    newMember.setPassword(prompt.inputString(
-        String.format("암호(%s)? ", oldMember.getPassword()), 
-        oldMember.getPassword()));
-    
-    newMember.setPhoto(prompt.inputString(
-        String.format("사진(%s)? ", oldMember.getPhoto()), 
-        oldMember.getPhoto()));
-    
-    newMember.setTel(prompt.inputString(
-        String.format("전화(%s)? ", oldMember.getTel()), 
-        oldMember.getTel()));
-    
-    if (oldMember.equals(newMember)) {
-      System.out.println("회원 변경을 취소하였습니다.");
-      return;
-    }
-    this.memberList.set(index, newMember);
-    System.out.println("회원을 변경했습니다.");
+    memberList.add(mem);
   }
-  
+
+
   public void deleteMember() {
-    int index = indexOfMember(prompt.inputInt("번호? "));
-    
-    if (index == -1) {
-      System.out.println("해당 번호의 회원이 없습니다.");
+    int no = prompt.inputInt("번호? ");
+    int index = indexOfMember(no);
+    if(index == -1) {
+      System.out.println("해당 회원을 찾을 수 없습니다.");
       return;
     }
-    
     this.memberList.remove(index);
-    
     System.out.println("회원을 삭제했습니다.");
   }
-  
-  private int indexOfMember(int no) {
-    for (int i = 0; i < this.memberList.size(); i++) {
-      if (this.memberList.get(i).getNo() == no) {
-        return i;
-      }
+
+
+  public void updateMember() {
+    int no = prompt.inputInt("번호? ");
+    int index = indexOfMember(no);
+
+    if(index == -1) {
+      System.out.println("해당 회원을 찾을 수 없습니다.");
+      return;
     }
-    return -1;
+
+    Member oldMember = this.memberList.get(index);
+    Member newMember = new Member();
+
+    //System.out.printf("번호? %d\n", oldMember.getNo());
+    newMember.setNo(oldMember.getNo());
+
+    newMember.setName(prompt.inputString(String.format(
+        "이름? (%s) ", oldMember.getName()),
+        oldMember.getName()));
+
+    newMember.setEmail(prompt.inputString(String.format(
+        "이메일? (%s) ", oldMember.getEmail()),
+        oldMember.getEmail()));
+
+    newMember.setPassword(prompt.inputString(String.format(
+        "비밀번호? (%s) ", oldMember.getPassword()),
+        oldMember.getPassword()));
+
+    newMember.setPhoto(prompt.inputString(String.format(
+        "사진? (%s) ", oldMember.getPhoto()),
+        oldMember.getPhoto()));
+
+    newMember.setTel(prompt.inputString(String.format(
+        "전화? (%s) ", oldMember.getTel()),
+        oldMember.getTel()));
+
+    newMember.setRegisteredDate(new Date(System.currentTimeMillis()));
+
+    if(oldMember.equals(newMember))
+      System.out.println("회원정보 변경을 취소했습니다");
+    else {
+      oldMember.setRegisteredDate(new Date(System.currentTimeMillis()));
+      memberList.set(index, newMember);
+      System.out.println("회원을 변경했습니다.");
+    }
   }
+
+
+  public void detailMember() {
+    int no = prompt.inputInt("번호? ");
+    int index = indexOfMember(no);
+
+    if(index == -1) {
+      System.out.println("해당 회원을 찾을 수 없습니다.");
+      return;
+    }
+    Member mem = memberList.get(index);
+
+    //System.out.printf("번호? %d\n", mem.getNo());
+    System.out.printf("이름? %s\n", mem.getName());
+    System.out.printf("이메일? %s\n", mem.getEmail());
+    System.out.printf("암호? %s\n", mem.getPassword());
+    System.out.printf("사진? %s\n", mem.getPhoto());
+    System.out.printf("전화? %s\n", mem.getTel());
+    System.out.printf("가입일? %tF\n",mem.getRegisteredDate());
+
+  }
+
+  private int indexOfMember(int no) {
+    for(int i = 0 ; i < this.memberList.size() ; i++)
+      if(this.memberList.get(i).getNo() == no)
+        return i; return -1;
+  }
+
 }
